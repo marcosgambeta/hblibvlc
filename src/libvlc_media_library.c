2796b7315a1993e03c24d8a0baa6fd75c0d1a222
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-02-14 10:52:19 -0300

    Add libvlc_media_library.c

---
+++ src/libvlc_media_library.c
@@ -0,0 +1,38 @@
+//
+// hblibvlc - Binding library for Harbour++/Harbour/xHarbour and libvlc
+//
+// Copyright (c) 2025 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+//
+
+#include <vlc/vlc.h>
+#include <hbapi.h>
+
+// LIBVLC_API libvlc_media_library_t * libvlc_media_library_new( libvlc_instance_t * p_instance )
+HB_FUNC(LIBVLC_MEDIA_LIBRARY_NEW)
+{
+  hb_retptr((libvlc_media_library_t *)libvlc_media_library_new((libvlc_instance_t *)hb_parptr(1)));
+}
+
+// LIBVLC_API void libvlc_media_library_release( libvlc_media_library_t * p_mlib )
+HB_FUNC(LIBVLC_MEDIA_LIBRARY_RELEASE)
+{
+  libvlc_media_library_release((libvlc_media_library_t *)hb_parptr(1));
+}
+
+// LIBVLC_API void libvlc_media_library_retain( libvlc_media_library_t * p_mlib )
+HB_FUNC(LIBVLC_MEDIA_LIBRARY_RETAIN)
+{
+  libvlc_media_library_retain((libvlc_media_library_t *)hb_parptr(1));
+}
+
+// LIBVLC_API int libvlc_media_library_load( libvlc_media_library_t * p_mlib )
+HB_FUNC(LIBVLC_MEDIA_LIBRARY_LOAD)
+{
+  hb_retni(libvlc_media_library_load((libvlc_media_library_t *)hb_parptr(1)));
+}
+
+// LIBVLC_API libvlc_media_list_t * libvlc_media_library_media_list( libvlc_media_library_t * p_mlib )
+HB_FUNC(LIBVLC_MEDIA_LIBRARY_MEDIA_LIST)
+{
+  hb_retptr((libvlc_media_list_t *)libvlc_media_library_media_list((libvlc_media_library_t *)hb_parptr(1)));
+}
