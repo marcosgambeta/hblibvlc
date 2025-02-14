1709149ef372254f649454dfb4fa4d06c2fbfbd3
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-02-14 20:36:34 -0300

    Add libvlc_renderer_discoverer.c

---
+++ src/libvlc_renderer_discoverer.c
@@ -0,0 +1,78 @@
+//
+// hblibvlc - Binding library for Harbour++/Harbour/xHarbour and libvlc
+//
+// Copyright (c) 2025 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+//
+
+#include <vlc/vlc.h>
+#include <hbapi.h>
+
+// LIBVLC_API libvlc_renderer_item_t * libvlc_renderer_item_hold(libvlc_renderer_item_t *p_item)
+HB_FUNC(LIBVLC_RENDERER_ITEM_HOLD)
+{
+  hb_retptr((libvlc_renderer_item_t *)libvlc_renderer_item_hold((libvlc_renderer_item_t *)hb_parptr(1)));
+}
+
+// LIBVLC_API void libvlc_renderer_item_release(libvlc_renderer_item_t *p_item)
+HB_FUNC(LIBVLC_RENDERER_ITEM_RELEASE)
+{
+  libvlc_renderer_item_release((libvlc_renderer_item_t *)hb_parptr(1));
+}
+
+// LIBVLC_API const char * libvlc_renderer_item_name(const libvlc_renderer_item_t *p_item)
+HB_FUNC(LIBVLC_RENDERER_ITEM_NAME)
+{
+  hb_retc((const char *)libvlc_renderer_item_name((const libvlc_renderer_item_t *)hb_parptr(1)));
+}
+
+// LIBVLC_API const char * libvlc_renderer_item_type(const libvlc_renderer_item_t *p_item)
+HB_FUNC(LIBVLC_RENDERER_ITEM_TYPE)
+{
+  hb_retc((const char *)libvlc_renderer_item_type((const libvlc_renderer_item_t *)hb_parptr(1)));
+}
+
+// LIBVLC_API const char * libvlc_renderer_item_icon_uri(const libvlc_renderer_item_t *p_item)
+HB_FUNC(LIBVLC_RENDERER_ITEM_ICON_URI)
+{
+  hb_retc((const char *)libvlc_renderer_item_icon_uri((const libvlc_renderer_item_t *)hb_parptr(1)));
+}
+
+// LIBVLC_API int libvlc_renderer_item_flags(const libvlc_renderer_item_t *p_item)
+HB_FUNC(LIBVLC_RENDERER_ITEM_FLAGS)
+{
+  hb_retni(libvlc_renderer_item_flags((const libvlc_renderer_item_t *)hb_parptr(1)));
+}
+
+// LIBVLC_API libvlc_renderer_discoverer_t * libvlc_renderer_discoverer_new( libvlc_instance_t *p_inst, const char *psz_name )
+HB_FUNC(LIBVLC_RENDERER_DISCOVERER_NEW)
+{
+  hb_retptr((libvlc_renderer_discoverer_t *)libvlc_renderer_discoverer_new((libvlc_instance_t *)hb_parptr(1), hb_parc(2)));
+}
+
+// LIBVLC_API void libvlc_renderer_discoverer_release( libvlc_renderer_discoverer_t *p_rd )
+HB_FUNC(LIBVLC_RENDERER_DISCOVERER_RELEASE)
+{
+  libvlc_renderer_discoverer_release((libvlc_renderer_discoverer_t *)hb_parptr(1));
+}
+
+// LIBVLC_API int libvlc_renderer_discoverer_start( libvlc_renderer_discoverer_t *p_rd )
+HB_FUNC(LIBVLC_RENDERER_DISCOVERER_START)
+{
+  hb_retni(libvlc_renderer_discoverer_start((libvlc_renderer_discoverer_t *)hb_parptr(1)));
+}
+
+// LIBVLC_API void libvlc_renderer_discoverer_stop( libvlc_renderer_discoverer_t *p_rd )
+HB_FUNC(LIBVLC_RENDERER_DISCOVERER_STOP)
+{
+  libvlc_renderer_discoverer_stop((libvlc_renderer_discoverer_t *)hb_parptr(1));
+}
+
+// LIBVLC_API libvlc_event_manager_t * libvlc_renderer_discoverer_event_manager( libvlc_renderer_discoverer_t *p_rd )
+HB_FUNC(LIBVLC_RENDERER_DISCOVERER_EVENT_MANAGER)
+{
+  hb_retptr((libvlc_event_manager_t *)libvlc_renderer_discoverer_event_manager((libvlc_renderer_discoverer_t *)hb_parptr(1)));
+}
+
+// LIBVLC_API size_t libvlc_renderer_discoverer_list_get( libvlc_instance_t *p_inst, libvlc_rd_description_t ***ppp_services )
+
+// LIBVLC_API void libvlc_renderer_discoverer_list_release( libvlc_rd_description_t **pp_services, size_t i_count )
