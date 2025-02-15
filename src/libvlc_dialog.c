0a0bae1d7e4adfdf381a7af11945963c65b95e53
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-02-14 21:07:00 -0300

    Add libvlc_dialog.c

---
+++ src/libvlc_dialog.c
@@ -0,0 +1,36 @@
+//
+// hblibvlc - Binding library for Harbour++/Harbour/xHarbour and libvlc
+//
+// Copyright (c) 2025 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+//
+
+#include <vlc/vlc.h>
+#include <hbapi.h>
+
+// LIBVLC_API void libvlc_dialog_set_callbacks(libvlc_instance_t *p_instance, const libvlc_dialog_cbs *p_cbs, void *p_data)
+
+// LIBVLC_API void libvlc_dialog_set_context(libvlc_dialog_id *p_id, void *p_context)
+HB_FUNC(LIBVLC_DIALOG_SET_CONTEXT)
+{
+  libvlc_dialog_set_context((libvlc_dialog_id *)hb_parptr(1), (void *)hb_parptr(2));
+}
+
+// LIBVLC_API void * libvlc_dialog_get_context(libvlc_dialog_id *p_id)
+HB_FUNC(LIBVLC_DIALOG_GET_CONTEXT)
+{
+  hb_retptr((void *)libvlc_dialog_get_context((libvlc_dialog_id *)hb_parptr(1)));
+}
+
+// LIBVLC_API int libvlc_dialog_post_login(libvlc_dialog_id *p_id, const char *psz_username, const char *psz_password, bool b_store)
+
+// LIBVLC_API int libvlc_dialog_post_action(libvlc_dialog_id *p_id, int i_action)
+HB_FUNC(LIBVLC_DIALOG_POST_ACTION)
+{
+  hb_retni(libvlc_dialog_post_action((libvlc_dialog_id *)hb_parptr(1), hb_parni(2)));
+}
+
+// LIBVLC_API int libvlc_dialog_dismiss(libvlc_dialog_id *p_id)
+HB_FUNC(LIBVLC_DIALOG_DISMISS)
+{
+  hb_retni(libvlc_dialog_dismiss((libvlc_dialog_id *)hb_parptr(1)));
+}
