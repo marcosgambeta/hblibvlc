f768ef913042482232a93ff8f9730be1d40fdec7
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-02-16 00:24:54 -0300

    Add macro libvlc_par_media_library

--- include/hblibvlc.h
+++ include/hblibvlc.h
@@ -20,6 +20,7 @@
 #define libvlc_par_int(n) hb_parni(n)
 #define libvlc_par_dialog_id(n) (libvlc_dialog_id *)hb_parptr(n)
 #define libvlc_par_int64(n) (int64_t)hb_parnl(n) // TODO: fix
+#define libvlc_par_media_library(n) (libvlc_media_library_t *)hb_parptr(n)
 
 // macros for return
 #define libvlc_ret_int(x) hb_retni(x)
@@ -29,4 +30,5 @@
 #define libvlc_ret_module_description(x) hb_retptr(x)
 #define libvlc_ret_int64(x) hb_retnl(x) // TODO: fix
 #define libvlc_ret_equalizer(x) hb_retptr(x)
+
 #endif // HBLIBVLC_H
--- src/libvlc_media_library.c
+++ src/libvlc_media_library.c
@@ -17,23 +17,23 @@ HB_FUNC(LIBVLC_MEDIA_LIBRARY_NEW)
 // LIBVLC_API void libvlc_media_library_release( libvlc_media_library_t * p_mlib )
 HB_FUNC(LIBVLC_MEDIA_LIBRARY_RELEASE)
 {
-  libvlc_media_library_release((libvlc_media_library_t *)hb_parptr(1));
+  libvlc_media_library_release(libvlc_par_media_library(1));
 }
 
 // LIBVLC_API void libvlc_media_library_retain( libvlc_media_library_t * p_mlib )
 HB_FUNC(LIBVLC_MEDIA_LIBRARY_RETAIN)
 {
-  libvlc_media_library_retain((libvlc_media_library_t *)hb_parptr(1));
+  libvlc_media_library_retain(libvlc_par_media_library(1));
 }
 
 // LIBVLC_API int libvlc_media_library_load( libvlc_media_library_t * p_mlib )
 HB_FUNC(LIBVLC_MEDIA_LIBRARY_LOAD)
 {
-  libvlc_ret_int(libvlc_media_library_load((libvlc_media_library_t *)hb_parptr(1)));
+  libvlc_ret_int(libvlc_media_library_load(libvlc_par_media_library(1)));
 }
 
 // LIBVLC_API libvlc_media_list_t * libvlc_media_library_media_list( libvlc_media_library_t * p_mlib )
 HB_FUNC(LIBVLC_MEDIA_LIBRARY_MEDIA_LIST)
 {
-  hb_retptr((libvlc_media_list_t *)libvlc_media_library_media_list((libvlc_media_library_t *)hb_parptr(1)));
+  hb_retptr((libvlc_media_list_t *)libvlc_media_library_media_list(libvlc_par_media_library(1)));
 }
