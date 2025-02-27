029d6c182a5347d2efa16df500dec615b50da1ef
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-02-27 08:02:28 -0300

    Update bcc32_makelib.bat and bcc64_makelib.bat

--- bcc32_makelib.bat
+++ bcc32_makelib.bat
@@ -3,8 +3,16 @@ rem batch to compile the library using xHarbour and BCC
 del lib\hblibvlc.lib
 
 bcc32 -O2 -d -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude src\libvlc.c
+bcc32 -O2 -d -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude src\libvlc_dialog.c
 bcc32 -O2 -d -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude src\libvlc_media.c
+bcc32 -O2 -d -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude src\libvlc_media_discoverer.c
+bcc32 -O2 -d -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude src\libvlc_media_library.c
+bcc32 -O2 -d -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude src\libvlc_media_list.c
+bcc32 -O2 -d -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude src\libvlc_media_list_player.c
 bcc32 -O2 -d -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude src\libvlc_media_player.c
+bcc32 -O2 -d -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude src\libvlc_renderer_discoverer.c
+bcc32 -O2 -d -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude src\libvlc_version.c
+bcc32 -O2 -d -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude src\libvlc_vlm.c
 
 for %%1 in (*.obj) do tlib /P64 lib\hblibvlc.lib +%%1
 
--- bcc64_makelib.bat
+++ bcc64_makelib.bat
@@ -2,9 +2,17 @@ rem batch to compile the library using xHarbour and BCC64
 
 del lib\hblibvlc64.a
 
-bcc64 -O2 -d -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude src\libvlc.c
-bcc64 -O2 -d -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude src\libvlc_media.c
-bcc64 -O2 -d -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude src\libvlc_media_player.c
+bcc64 -O2 -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude -Wno-typedef-redefinition src\libvlc.c
+bcc64 -O2 -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude -Wno-typedef-redefinition src\libvlc_dialog.c
+bcc64 -O2 -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude -Wno-typedef-redefinition src\libvlc_media.c
+bcc64 -O2 -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude -Wno-typedef-redefinition src\libvlc_media_discoverer.c
+bcc64 -O2 -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude -Wno-typedef-redefinition src\libvlc_media_library.c
+bcc64 -O2 -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude -Wno-typedef-redefinition src\libvlc_media_list.c
+bcc64 -O2 -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude -Wno-typedef-redefinition src\libvlc_media_list_player.c
+bcc64 -O2 -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude -Wno-typedef-redefinition src\libvlc_media_player.c
+bcc64 -O2 -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude -Wno-typedef-redefinition src\libvlc_renderer_discoverer.c
+bcc64 -O2 -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude -Wno-typedef-redefinition src\libvlc_version.c
+bcc64 -O2 -c -I%HB_PATH%\include -I%LIBVLC_INC_DIR% -Iinclude -Wno-typedef-redefinition src\libvlc_vlm.c
 
 for %%1 in (*.o) do tlib64 /P64 lib\hblibvlc64.a +%%1
 
